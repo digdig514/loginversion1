b85d0dc1d13c786870a7059f0e8ff965579533df
Diego Souza Sampaio <digdig@MacBook-Air-de-Diego.local>
2017-02-20 16:53:48 -0500

    Initial Commit

---
+++ LoginAccess/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginAccess
+//
+//  Created by Diego Souza Sampaio on 17-02-20.
+//  Copyright © 2017 Diego Souza Sampaio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
